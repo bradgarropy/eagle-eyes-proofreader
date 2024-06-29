e8bca964ce99c2018571fad7f3e4c6bfa12aba1a
Brad Garropy <bgarropy@atlassian.com>
2024-06-28 20:45:25 -0500

    add coming soon page.

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -2,15 +2,16 @@ import type {MetaFunction} from "@remix-run/node"
 
 export const meta: MetaFunction = () => [
     {
-        title: "remix starter | home",
+        title: "Eagle Eyes",
     },
 ]
 
 const IndexRoute = () => {
     return (
-        <>
-            <h2 className="text-2xl font-bold">Home</h2>
-        </>
+        <div className="w-screen h-screen grid place-items-center content-center">
+            <h1 className="text-2xl font-bold">Eagle Eyes Proofreader</h1>
+            <p>Coming soon</p>
+        </div>
     )
 }
 
